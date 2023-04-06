1f3c93edf4652ffd29805d4e0140356a2ad599e9
Artur Henrique <arthenrdev@gmail.com>
2023-04-06 11:29:06 -0300

    fix: Desafio 4

--- challenges/desafio4.sql
+++ challenges/desafio4.sql
@@ -1,14 +1,11 @@
 SELECT 
-    u.nome AS 'pessoa_usuaria',
+    u.nome AS pessoa_usuaria,
     IF(MAX(YEAR(h.data_reproducao)) >= 2021,
         'Ativa',
-        'Inativa') AS 'status_pessoa_usuaria'
+        'Inativa') AS status_pessoa_usuaria
 FROM
     SpotifyClone.usuario AS u
         INNER JOIN
     SpotifyClone.historico_reproducao AS h ON u.user_id = h.user_id
 GROUP BY pessoa_usuaria
 ORDER BY pessoa_usuaria ASC;
-
--- test
---test 

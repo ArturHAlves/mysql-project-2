b3d13f461292a3789cb44e62c053fe64551d7f89
Artur Henrique <arthenrdev@gmail.com>
2023-04-06 10:46:52 -0300

    feat: Req 4 - Query que mostra user ativos a partir de 2021

---
+++ challenges/desafio4.sql
@@ -0,0 +1,12 @@
+SELECT 
+    u.nome AS 'pessoa_usuaria',
+    IF(MAX(YEAR(h.data_reproducao)) >= 2021,
+        'Ativa',
+        'Inativa') AS 'status_pessoa_usuaria'
+FROM
+    SpotifyClone.usuario AS u
+        INNER JOIN
+    SpotifyClone.historico_reproducao AS h ON u.user_id = h.user_id
+GROUP BY pessoa_usuaria
+ORDER BY pessoa_usuaria ASC;
+
